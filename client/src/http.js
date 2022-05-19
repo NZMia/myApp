2b4bce05deadc43adc3a5c1376c9387b563cad9b
Mia.Zhang <mia7zwy@hotmail.com>
2022-05-19 13:53:59 +1200

    build: baseURL updates

--- client/src/http.js
+++ client/src/http.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-// const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const api = axios.create({
-  baseURL: 'https://api.github.com/users/',
+  baseURL: API_URL,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
