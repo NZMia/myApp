7edbe4346328a5cb4f027c142a2a3695706b1841
Mia.Zhang <mia7zwy@hotmail.com>
2022-05-19 13:59:31 +1200

    revert: baseURL issue

--- client/src/http.js
+++ client/src/http.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+console.info('API_URL', API_URL);
+
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://api.github.com/users/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
