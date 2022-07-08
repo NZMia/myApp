33a5a8f5e5d605cf514f4e03b035dabc554e1d3d
Mia.Zhang <mia7zwy@hotmail.com>
2022-07-08 13:17:15 +1200

    feat: header setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import logo from '../images/logo.png';
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src={logo} alt="" />
+      </div>
+      <div className="header__right">
+        <button>Admin</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
