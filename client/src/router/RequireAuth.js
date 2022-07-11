7a9933a55fd1b98bf1e297eed2ac9c60c4799002
Mia.Zhang <mia7zwy@hotmail.com>
2022-07-11 21:05:54 +1200

    refactor: login

--- client/src/router/RequireAuth.js
+++ client/src/router/RequireAuth.js
@@ -2,15 +2,8 @@ import { useSelector } from 'react-redux';
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 
 const RequireAuth = () => {
-  const { credential, token } = useSelector((state) => state.user);
+  const { credential } = useSelector((state) => state.user);
   const location = useLocation();
-
-  // console.info('currentUser', currentUser);
-  // console.info('loading', loading);
-
-  // if (loading) {
-  //   return 'loading.....';
-  // }
 
   return credential?.email ? (
     <Outlet />
