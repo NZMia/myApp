787254d457345ed97c8b17defa8af69f7aa1118d
Mia.Zhang <mia7zwy@hotmail.com>
2022-07-12 11:28:58 +1200

    ci: added redux-logger

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,13 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
+import logger from 'redux-logger';
+
 import { baseApi } from './api/baseApi';
 import userApi from './api/userApi';
 
 import userSlice from './slice/userSlice';
 
-const middlewareHandler = () => {
-  const middlewareList = [userApi.middleware];
+const middlewareHandler = (getDefaultMiddleware) => {
+  const middlewareList = [...getDefaultMiddleware(), userApi.middleware];
+
+  if (process.env.NODE_ENV === 'development') {
+    middlewareList.push(logger);
+  }
 
   return middlewareList;
 };
@@ -17,8 +23,7 @@ export const rootStore = configureStore({
     [baseApi.reducerPath]: baseApi.reducer,
     user: userSlice
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+  middleware: (getDefaultMiddleware) => middlewareHandler(getDefaultMiddleware),
   devTools: true
 });
 
